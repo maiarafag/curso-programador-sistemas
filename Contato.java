b5346e8ccf6575bb9ca89f54f351b2ddc78ce18a
mayaraf15 <maiara.fag@hotmail.com>
2022-03-17 10:40:59 -0300

    primeiro commit

---
+++ Contato.java
@@ -0,0 +1,113 @@
+package com.mycompany.agendatelefonica;
+
+public class Contato {
+    //Atributos
+    
+    private int codigo;
+    private String nome;
+    private String endereco;
+    private String cidade;
+    private String estado;
+    private String telefone;
+    private String celular;
+    private String dataNasc;
+    private String email;
+    private String comentario;
+    
+    //Métodos
+    
+    public void incluir(){
+        
+    }
+    public void salvar(){
+        
+    }
+    public void excluir(){
+        
+    }
+    public void sair(){
+        
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public void setDataNasc(String dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public String getDataNasc() {
+        return dataNasc;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+    
+    
+}
